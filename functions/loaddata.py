d07933b66b2881786bf502af7bc65e6f0881361f
Fraukje <fraukje.coopmans@hu.nl>
2023-11-29 14:59:33 +0100

    add accuracy test

--- functions/loaddata.py
+++ functions/loaddata.py
@@ -1,10 +1,12 @@
 import pandas as pd
 # words = pd.DataFrame(dw.get_ranked())
 # import dutch_words as dw
-
+import os
 import nltk
-
-df = pd.read_csv(r'nse2023openant.csv', sep = ';')
+logedin_user = os.getlogin()
+path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
+file_name = "nse2023openant.csv"
+df = pd.read_csv(f'{path}{file_name}', sep =';')
 
 words = pd.read_csv(r'files\wordlist.txt', sep = ';')
 words['AVG'] = 0
@@ -19,7 +21,7 @@ import re
 df = df[~df['antwoord_clean'].isnull()]
 
 df['AVG'] = 0
-for i in range(1,1000):
+for i in range(1,100):
     test = df['antwoord_clean'][i]
 
     test_df = pd.DataFrame({'WoordenClean':re.findall(r'(\w+)', test)})
@@ -29,9 +31,9 @@ for i in range(1,1000):
     check_df['AVG'] = np.where(check_df['AVG'].isnull(),1,0)
 
     if check_df['AVG'].sum() >= 1:
-        df['AVG'][i] = 1
+        df.loc[i,'AVG'] = 1
     else:
-        df['AVG'][i] = 0
+        df.loc[i,'AVG'] = 0
     print(f'{round(i/1000*100,0)}%')
 
 print('done')
--- functions/validation.py
+++ functions/validation.py
@@ -1,10 +1,11 @@
 ## Input is DF with at least the following columns:
-# 1. Succesfully anonimised column = 1 or 0, 
-# 2. AVG = 1 or 0 (whether )
-# efficiency --> number of anonimised cases / total cases
+# 1. 'seaa_avg': determined whether column has AVG data = 1 or 0, 
+# 2. AVG = 1 or 0 (whether column actually has AVG data)
+# efficiency --> number of detected cases without AVG / total cases
 # accuracy   --> number of correctly detected cases / total cases
 
 import numpy as np  #Needed for random number generator
+import pandas as pd
 
 # Load data
 logedin_user = os.getlogin()
@@ -12,8 +13,37 @@ path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Ana
 file_name = "nse2023openant.csv"
 nseant_df = pd.read_csv(f'{path}{file_name}', sep =';')
 
-# Create fake data column 'Success' 
-nseant_df['Success'] = np.random.randint(0,2, nseant_df.shape[0])
+# Add fake data column 'seaa_avg' (with random value 0 or 1) 
+# --> replace with run of SEAA when ready
+nseant_df['seaa_avg'] = np.random.randint(0,2, nseant_df.shape[0])
 
 # Calculate efficiency
-efficiency = sum(nseant_df['Success']) / len(nseant_df)
+efficiency = sum(nseant_df['seaa_avg']) / len(nseant_df)
+
+## Calculate accuracy
+# Load accuracy testdata
+acctestdata_df = pd.read_csv(f'{path}{"fake data nse open vragen.csv"}', sep =';')
+
+#Run SEAA on accuracy testdata 
+# Add fake data column 'seaa_avg' (with random value 0 or 1) 
+# --> replace with run of SEAA when ready
+acctestdata_df['seaa_avg'] = np.random.randint(0,2, acctestdata_df.shape[0])
+
+# Calculate accuracy
+# When seaa_avg is 1, it means that SEAA  determined the string to be containing AVG data. 
+# This is true when the actual string did contain any privacy-related 
+# fields (AVG = 1).
+# When seaa_avg is 0, SEAA determined that the string is AVG-free, so it will not
+# contain AVG data (AVG = 0).
+#
+# When assessing the algorithm SEAA the output of SEAA for a case falls 
+# into one of the following four categories:
+# True positive: seaa_avg = 1 and AVG = 1
+# True negative: seaa_avg = 0 and AVG = 0
+# False positive: seaa_avg = 1 and AVG = 0
+# False negative = seaa_avg = 0 and AVG = 1
+#
+# Accuracy = (true positives + true negatieve) / total cases
+true_positives = sum((acctestdata_df['AVG?']==1) & (acctestdata_df['seaa_avg'] == 1));
+true_negatives = sum((acctestdata_df['AVG?']==0) & (acctestdata_df['seaa_avg'] == 0));
+accuracy = (true_positives + true_negatives) / len(acctestdata_df)
