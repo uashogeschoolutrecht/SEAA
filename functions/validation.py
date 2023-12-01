76cfd45bcec21ca6cb8587a7e79e5f4fa91ee5d6
Fraukje <fraukje.coopmans@hu.nl>
2023-12-01 13:38:08 +0100

    add actual data

--- functions/validation.py
+++ functions/validation.py
@@ -1,33 +1,29 @@
 ## Input is DF with at least the following columns:
-# 1. 'seaa_avg': determined whether column has AVG data = 1 or 0, 
-# 2. AVG = 1 or 0 (whether column actually has AVG data)
+# 1. AVG: classification of SEAA algorithm on whether row has AVG data = 1 or 0, 
+# 2. actual__AVG = 1 or 0 (whether column actually has AVG data)
 # efficiency --> number of detected cases without AVG / total cases
 # accuracy   --> number of correctly detected cases / total cases
 
 import numpy as np  #Needed for random number generator
 import pandas as pd
+import os
 
-# Load data
-logedin_user = os.getlogin()
-path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
-file_name = "nse2023openant.csv"
-nseant_df = pd.read_csv(f'{path}{file_name}', sep =';')
+# Get data that was run through SEAA
+nseant_df = df;
 
-# Add fake data column 'seaa_avg' (with random value 0 or 1) 
-# --> replace with run of SEAA when ready
-nseant_df['seaa_avg'] = np.random.randint(0,2, nseant_df.shape[0])
-
-# Calculate efficiency
-efficiency = sum(nseant_df['seaa_avg']) / len(nseant_df)
+# Calculate efficiency (%)
+efficiency = (1 - (sum(nseant_df['AVG']) / len(nseant_df)))*100
 
 ## Calculate accuracy
 # Load accuracy testdata
+logedin_user = os.getlogin()
+path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
 acctestdata_df = pd.read_csv(f'{path}{"fake data nse open vragen.csv"}', sep =';')
 
 #Run SEAA on accuracy testdata 
 # Add fake data column 'seaa_avg' (with random value 0 or 1) 
 # --> replace with run of SEAA when ready
-acctestdata_df['seaa_avg'] = np.random.randint(0,2, acctestdata_df.shape[0])
+acctestdata_df['AVG'] = np.random.randint(0,2, acctestdata_df.shape[0])
 
 # Calculate accuracy
 # When seaa_avg is 1, it means that SEAA  determined the string to be containing AVG data. 
