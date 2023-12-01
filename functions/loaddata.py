c2c3ee79aff9bc2c86909263a832dcb1e55e0330
Fraukje <fraukje.coopmans@hu.nl>
2023-12-01 13:11:29 +0100

    Add comments

--- functions/loaddata.py
+++ functions/loaddata.py
@@ -1,33 +1,34 @@
 import pandas as pd
-# words = pd.DataFrame(dw.get_ranked())
-# import dutch_words as dw
 import os
 import nltk
+import numpy as np
+import re
+
+# Import data
 logedin_user = os.getlogin()
 path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
 file_name = "nse2023openant.csv"
 df = pd.read_csv(f'{path}{file_name}', sep =';')
 
+# Import dictionary of Dutch words
 words = pd.read_csv(r'files\wordlist.txt', sep = ';')
 words['AVG'] = 0
-# cleaning
+
+# Clean both dataframes (data + dictionary)
 df['antwoord_clean'] = df['Antwoord'].str.lower() 
 words['WoordenClean'] = words['WoordenClean'].str.lower() 
 words = words.drop_duplicates()
 words.reset_index(inplace=True,drop=True)
 
-import numpy as np
-import re
+# Remove null values
 df = df[~df['antwoord_clean'].isnull()]
 
+# For each string, determine whether it is contains privacy-related (AVG) data.
 df['AVG'] = 0
 for i in range(1,100):
     test = df['antwoord_clean'][i]
-
     test_df = pd.DataFrame({'WoordenClean':re.findall(r'(\w+)', test)})
-
     check_df = pd.merge(test_df,words,'left')
-    
     check_df['AVG'] = np.where(check_df['AVG'].isnull(),1,0)
 
     if check_df['AVG'].sum() >= 1:
