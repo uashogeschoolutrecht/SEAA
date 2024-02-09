1507bc220520971e296450256cc72558d1231566
PimLamberts <pim.lamberts@hu.nl>
2024-02-09 15:36:42 +0100

    Unieke AVG gevoelige woorden gefilterd. count per AVG gevoelig woord toegevoegd. Export naar avg_words_count.csv.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os 
+import pandas as pd
 from functions.loadSEAAdata import loaddata
 from functions.loadSEAAdata import loaddict
 
@@ -8,11 +9,11 @@ if logedin_user == 'pim.lamberts': #User Pim does not see the parent folder
     path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Open antwoorden\\"
 else:
     path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
-file_name = "nse2023openant.csv"
+file_name = "data\\nse2023openant.csv"
 nseant_df = loaddata(path, file_name)
 
 # import Dutch word list dictionary
-dictionary = "wordlist.txt"
+dictionary = "dict\\wordlist.txt"
 word_list_df = loaddict(path, dictionary)
 
 from functions.SEAA import SEAA
@@ -23,30 +24,25 @@ from functions.validation import SEAA_efficiency
 SEAA_efficiency(result_df)
 
 # Calculate accuracy of SEAA
-validation_df = loaddata(path, "nse annoteringen.csv")
+validation_df = loaddata(path, "data\\nse annoteringen.csv")
 
 from functions.validation import SEAA_accuracy
 SEAA_accuracy(validation_df, word_list_df)
 
-# Create a list of all 'gevoelige woorden'
-col_avg_list = result_df['gevoelige_woorden'].tolist()
-col_avg_list = [i for i in col_avg_list if i]
-avg_list = [sub_item for item in col_avg_list for sub_item in item.split(", ")]
-unique_avg_list = []
 
-# Keep unique 'gevoelige woorden'
-# Improvement: add count per unique value
-for word in avg_list:
-    if word not in unique_avg_list:
-        unique_avg_list.append(word)
+#Retrieve list of flagged AVG words
+avg_list = result_df['gevoelige_woorden'].tolist()
+while('' in avg_list):
+    avg_list.remove('')
 
-# Write list to csv file
-import csv
-csv_file_path = 'unique_avg_words.csv'
+avg_list_def = []
+for item in avg_list:
+    words = item.split(", ")
+    avg_list_def.extend(words)
 
-with open(csv_file_path, 'w', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile)
-    for item in unique_avg_list:
-        csv_writer.writerow([item])
+# Create datframe containing all AVG words with count
+word_count = pd.Series(avg_list_def).value_counts()
+avg_words_df = pd.DataFrame({'AVG_woord': word_count.index, 'Count': word_count.values})
 
-
+csv_file_path = 'avg_words_count.csv'
+avg_words_df.to_csv(csv_file_path, index=False) 
