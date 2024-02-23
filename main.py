3b7173255582c96060a4ecbdc6a33982ed8b0b26
ALeemans <anne.leemans@live.nl>
2024-02-23 12:51:55 +0100

    small changes

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+import os 
+import pandas as pd
+from functions.loadSEAAdata import loaddata
+from functions.loadSEAAdata import loaddict
+
+# import NSE open answers
+logedin_user = os.getlogin()
+if logedin_user == 'pim.lamberts': #User Pim does not see the parent folder
+    path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Open antwoorden\\"
+else:
+    path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
+file_name = "nse2023openant.csv"
+nseant_df = loaddata(path, file_name)
+
+# import dictionaries
+# Dutch word dictionary
+dictionary = "wordlist.txt"
+word_list_df = loaddict(path, dictionary)
+
+# white list (words not part of the Dutch dictionary but considered safe regardless)
+dictionary = 'whitelist.txt'
+whitelist_df = loaddict(path, dictionary)
+word_list_df = pd.concat([word_list_df, whitelist_df], ignore_index=True)
+
+# illnesses dictionary
+dictionary = 'illness.txt'
+illness_df = loaddict(path, dictionary, 'illness')
+
+# Run SEAA
+from functions.SEAA import SEAA
+result_df = SEAA(nseant_df, word_list_df,illness_df)
+
+
+# Run SEAA Max Performance 
+
 from functions.SEAA_performance import SEAA
 
 # Run model on preselected lists
@@ -21,8 +56,27 @@ result_df = SEAA(
     dict_col="words",
 
     # Define numer of lines for test run !NOTE set to 0 for a FULL run
-    testrun=500,
+    testrun=0,
 )
+
+from functions.validation import SEAA_efficiency
+# Calculate efficiency of SEAA
+efficiency = SEAA_efficiency(result_df)
+
+# Calculate accuracy of SEAA
+validation_df = loaddata(path, "nse annoteringen.csv")
+
+from functions.validation import SEAA_accuracy
+accuracy = SEAA_accuracy(validation_df, word_list_df, illness_df)
+
+#Extract AVG words with count
+from AVG_list import AVG_list
+avg_words_df = AVG_list(result_df)
+
+# Save word count list to file
+file_name = 'avg_words_count.csv'
+avg_words_df.to_csv(f'{path}{file_name}', sep =';')
+
 
 
 
---
+++ results/results_sprint_23_C2.md
@@ -0,0 +1,8 @@
+# Pilot SEAA - 2 C 2023 (15-02-23 - 18-02-2023)
+
+## Work done
+Performance optimazation, instead of search in list AVG words are identified by merging. All words from an awnser are transformed to a dataframe and merged with the 'save' words list. NA values are marked as sensitive words. 
+
+## Results 
+Total run time previous model (search in list): 
+Total run time new model (merge): 
