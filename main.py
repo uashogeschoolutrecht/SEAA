37020b9cac9cb002da09c04dbb676e137b69078e
Fraukje <fraukje.coopmans@hu.nl>
2024-03-25 14:46:13 +0100

    functionize whitelisting by user

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if logedin_user == 'pim.lamberts': #User Pim does not see the parent folder
     path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Open antwoorden\\"
 else:
     path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
-file_name = "nse annoteringen totaal.csv"
+file_name = "nse2023openant.csv"
 nseant_df = loaddata(path, file_name)
 
 # import dictionaries
@@ -31,7 +31,7 @@ flag_df = pd.concat([illness_df, blacklist_df], ignore_index=True)
 
 # Run SEAA
 from functions.SEAA import SEAA
-result_df = SEAA(nseant_df, word_list_df,flag_df) # <== 4m .7s
+result_df = SEAA(nseant_df, word_list_df,flag_df, 50) # <== 4m .7s
 
 # Add Dutch or not Dutch column classificatiion
 # If the anwser contains 8 or more words and more than 40 percent of those words are unkown
@@ -67,36 +67,8 @@ avg_words_df= avg_words_df.merge(flag_df,'left', left_on='AVG_woord',right_on='w
 # remove rows with blacklisted words and remove redundant columns
 avg_words_df = avg_words_df[avg_words_df['words'].isna()].drop(columns='words')
 
-def jaNeeInput(question):
-    ## Let user choose input only 'j' or 'n'.
-    while "the answer is invalid":
-        reply = str(input(question+' (j/n/blacklist): ')).lower().strip()
-        if reply[0] == 'j':
-            return 'j'
-        elif reply == 'blacklist':
-            return 'b'
-        elif reply[0] == 'n':
-            return 'n'
-        else:
-            print('Onjuiste input antwoord moet j/n/blacklist zijn!')
-
-# Let user check words and add to whitelist or blacklist based on user input 
-for i in avg_words_df.head(15).index:
-    avg_woord = avg_words_df['AVG_woord'][i]
-    #Get user input and transform to dataframe
-    user_input = jaNeeInput(f"{avg_woord} kwam {avg_words_df['Count'][i]} keer voor in de open antwoorden.\nWil je dit woord toevoegenaan de whitelist? ")
-    antwoord_df = pd.DataFrame({'words': [avg_woord]})
-    
-    if user_input == 'j':
-        #If answer is yes add to whitelist
-        whitelist_df = pd.concat([whitelist_df, antwoord_df], axis=0)
-        print(f'Woord "{avg_woord}" is toegevoegd aan de whitelist')
-    elif user_input == 'blacklist':
-        #If answer is blacklist: add to blacklist
-        blacklist_df = pd.concat([blacklist_df, antwoord_df], axis=0)
-        print(f'Woord "{avg_woord}" is toegevoegd aan de blacklist')
-    else:
-        print(f'Woord "{avg_woord}" is overgeslagen')
+from functions.expand_dicts import expand_dicts
+whitelist_df, blacklist_df = expand_dicts(avg_words_df, whitelist_df, blacklist_df)
 
 whitelist_df.to_csv(f'{path}//dict//whitelist.txt',index=False)
 blacklist_df.to_csv(f'{path}//dict//blacklist.txt',index=False)
