0e406eef5ad2e9647662d553c4f6d8929f1f2236
Fraukje <fraukje.coopmans@hu.nl>
2024-08-28 15:09:32 +0200

    Remove exception for Pim

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from functions.loadSEAAdata import loaddata
 
 ## Import NSE open answers
 logedin_user = os.getlogin()
-if logedin_user == 'pim.lamberts': #User Pim does not see the parent folder
-    path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Open antwoorden\\"
-else:
-    path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
+path = f"C:\\Users\\{logedin_user}\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Open antwoorden\\"
 file_name = "nse2023openant.csv"
 nseant_df = loaddata(path, file_name)
 
