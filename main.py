2e1041e0fafd10ea9f884a001ae94ef4a573f0b2
ALeemans <anne.leemans@live.nl>
2026-05-12 13:17:14 +0200

    Fix input and output file paths in main.py for updated data processing

--- main.py
+++ main.py
@@ -134,9 +134,9 @@ def _load_flag_words() -> pd.DataFrame:
 
 if __name__ == "__main__":
     # Example usage - modify these paths as needed
-    INPUT_FILE = r'nse_2024.csv'
+    INPUT_FILE = r'NSE_2026_transformed.csv'
     INPUT_FOLDER = r"C:\Users\AnneL\Stichting Hogeschool Utrecht\FCA-DA-P - Inleesbestanden\Domein Education Analytics\SEAA\NSE\input"
-    OUTPUT_FOLDER = r"C:\Users\AnneL\Stichting Hogeschool Utrecht\FCA-DA-P - Inleesbestanden\Domein Education Analytics\SEAA\NSE\output"
+    OUTPUT_FOLDER = r"C:\Users\AnneL\Stichting Hogeschool Utrecht\FCA-DA-P - Inleesbestanden\Domein Education Analytics\SEAA\NSE\ounput"
     ANSWER_COLUMN = 'Antwoord'
 
     process_answers(
